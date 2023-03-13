416c857d39d231dcb15311f081c3fb1c09b15cf6
Théo Maillet--Seeuws <forret.thibault@gmail.com>
2023-03-13 10:34:23 +0100

    Ajout de commentaire

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/***
+Rôle : Renvoie le carré d'un nombre passé en paramètre
+Paramètre d'entrée : Nombre entier
+Valeur de retour : Le carré du nombre passé en paramètre
 
+***/
 int RenvoieCarre(int i) {
 	return i*i;
 }
